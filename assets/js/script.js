dd69f51c0084158ce6df1e31b3ea335a9e30d3c0
Rodrigo Barros <rodrigobarros2802@gmail.com>
2023-07-02 12:28:11 -0300

    Adicionando back ground #333

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,5 @@ let banner = document.querySelector('.banner');
 let dayNight = document.querySelector('.dayNight');
 
 dayNight.onclick = function() {
-    
+    banner.classList.toggle('night');
 } 
